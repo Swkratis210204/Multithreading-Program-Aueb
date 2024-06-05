2de5d2af2ba1a2cd3717287df9444a68f0922554
Swkratis210204 <149266430+Swkratis210204@users.noreply.github.com>
2024-06-05 22:03:06 +0300

    Create p3220028-p3220031.h

---
+++ p3220028-p3220031.h
@@ -0,0 +1,38 @@
+
+// p3220028-p3220031.h
+
+#ifndef p3220028-p3220031
+#define p3220028-p3220031
+
+#define Ntel 2
+#define Ncook 2
+#define Noven 10
+#define Ndeliverer 10
+
+#define Torderlow 1
+#define Torderhigh 5
+
+#define Norderlow 1
+#define Norderhigh 5
+
+#define Pm 35
+#define Pp 25
+#define Ps 40
+
+#define Tpaymentlow 1
+#define Tpaymenthigh 3
+
+#define Pfail 5
+
+#define Cm 10
+#define Cp 11
+#define Cs 12
+
+#define Tprep 1
+#define Tbake 10
+#define Tpack 1
+
+#define Tdellow 5
+#define Tdelhigh 15
+
+#endif 
